16ddb19cf09f78055ee46a95bdb0a20d7d7864ed
李玉龙 <919188649@qq.com>
2018-11-19 15:14:29 +0800

    483题最小好进制1
    对于给定的整数 n, 如果n的k（k>=2）进制数的所有数位全为1，则称 k（k>=2）是 n 的一个好进制。
    
    以字符串的形式给出 n, 以字符串的形式返回 n 的最小好进制。
    
    示例 1：
    
    输入："13"
    输出："3"
    解释：13 的 3 进制是 111。
    示例 2：
    
    输入："4681"
    输出："8"
    解释：4681 的 8 进制是 11111。
    示例 3：
    
    输入："1000000000000000000"
    输出："999999999999999999"
    解释：1000000000000000000 的 999999999999999999 进制是 11。

---
+++ 483SmallestGoodBase.py
@@ -0,0 +1,24 @@
+import math
+class Solution(object):
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        num = int(n)
+        thisLen = int(math.log(num,2)) + 1
+        while thisLen > 2:
+            # from equation [3], we havve
+            thisBase = int(num ** (1.0/(thisLen - 1)))
+            # from equation [2], we have
+            if num * (thisBase - 1) == thisBase ** thisLen - 1:
+                return str(thisBase)
+            thisLen -= 1
+        return str(num - 1)
+
+if __name__ == '__main__':
+    n=input("n=")
+    l=Solution.smallestGoodBase(n,n)
+    print(l)
+
+
