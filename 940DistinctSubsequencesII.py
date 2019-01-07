445e0b3ae932d1ac9879a76d564ab1c88c7e8f43
mojingdada <44856207+mojingdada@users.noreply.github.com>
2019-01-07 22:03:59 +0800

    Update 940DistinctSubsequencesII.py
    给定一个字符串 S，计算 S 的不同非空子序列的个数。
    
    因为结果可能很大，所以返回答案模 10^9 + 7.

--- 940DistinctSubsequencesII.py
+++ 940DistinctSubsequencesII.py
@@ -1,19 +1,4 @@
 from itertools import combinations
-# class Solution:
-#     def distinctSubseqII(self, S):
-#         """
-#         :type S: str
-#         :rtype: int
-#         """
-#         n=len(S)
-#         queue=[]
-#         for i in range(1,n+1):
-#            queue.append(set(combinations(S,i)))
-#         n1=len(queue)
-#         num=0
-#         for j in range(n1):
-#             num+=len(queue[j])
-#         return num
 class Solution:
     def distinctSubseqII(self, S):
         """
